edc31a59e27169e20fc1fa1c51d08e6b87da9c2f
Jirayu-Kaewnongkhoon <samurai_ice007@hotmail.com>
2019-11-02 22:11:08 +0700

    ui forgot password

--- app/src/main/java/com/example/seapp/MainActivity.java
+++ app/src/main/java/com/example/seapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.seapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView forgotPass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //forgotPass.setPaintFlags(forgotPass.getPaintFlags() |   Paint.UNDERLINE_TEXT_FLAG);
+
+        forgotPass = findViewById(R.id.tvForgotPass);
+        forgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ForgotPassword.class));
+            }
+        });
     }
 }
