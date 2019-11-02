dcc4e60d3e3799b6ba14bf3f6c271e8c54228c9a
Phanukorn <60050230@kmitl.ac.th>
2019-11-02 12:01:54 +0700

    ฺRegister v.1.01
    แก้ไข ปัญหากดปุ่ม navigation

--- app/src/main/java/com/example/seapp/Register.java
+++ app/src/main/java/com/example/seapp/Register.java
@@ -1,12 +1,17 @@
 package com.example.seapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Register extends AppCompatActivity  {
 
@@ -14,12 +19,37 @@ public class Register extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        FragmentA fragment = new FragmentA();
-        transaction.add(R.id.frame,fragment);
-        transaction.addToBackStack("fragmentA");
-        transaction.commit();
 
+
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction transaction = fragmentManager.beginTransaction();
+//        FragmentA fragment = new FragmentA();
+//        transaction.add(R.id.frame,fragment);
+//        transaction.addToBackStack("fragmentA");
+//        transaction.commit();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame,new KmitlFragment()).commit();
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectFragment = null;
+
+            switch (menuItem.getItemId()){
+                case R.id.KMITL:
+                    selectFragment = new KmitlFragment();
+                    break;
+                case R.id.guest:
+                    selectFragment = new GuestFragment();
+                    break;
+            }//switch
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame,selectFragment).commit();
+
+            return  true;
+        }//OnNavigation
+    };
 }
