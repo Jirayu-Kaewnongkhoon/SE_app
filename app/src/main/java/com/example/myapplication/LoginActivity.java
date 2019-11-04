2a1dea00d99c2bde2517c7d3bb6c4fa5c45fc5b7
Khanisorn Inchuay <ks_khanisorn_dear@hotmail.com>
2019-11-04 17:42:16 +0700

    cs talk by dear

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -6,12 +6,12 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         TextView text = findViewById(R.id.textView2);
         text.setPaintFlags(text.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
