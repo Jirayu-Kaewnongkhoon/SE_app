c56977401e5d47efb03fcf5afc4df224d0050840
60050230 <dragonfile005@hotmail.com>
2019-11-04 14:16:20 +0700

    Report Layout V.1

--- app/src/main/java/com/example/seapp/Report.java
+++ app/src/main/java/com/example/seapp/Report.java
@@ -3,12 +3,51 @@ package com.example.seapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Report extends AppCompatActivity {
+    private RadioGroup radio;
+    private EditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
-    }
+
+
+        text = (EditText)findViewById(R.id.other_textfiled);
+        radio = (RadioGroup) findViewById(R.id.radioGroup);
+        radio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                View radioButton = radio.findViewById(checkedId);
+                int index = radio.indexOfChild(radioButton);
+                switch (index) {
+                    case 0: // first button
+                        text.setVisibility(View.INVISIBLE);
+                        break;
+                    case 1: // secondbutton
+                        text.setVisibility(View.INVISIBLE);
+                        break;
+                    case 2:
+                        text.setVisibility(View.INVISIBLE);
+                        break;
+                    case 3:
+                        text.setVisibility(View.INVISIBLE);
+
+                        break;
+                    case 4:
+                        text.setVisibility(View.VISIBLE);
+                        break;
+                }
+
+            }
+        });//setOncheck
+
+
+    }//Oncreate
 }
