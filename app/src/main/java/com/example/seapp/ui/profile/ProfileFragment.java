f68b82a131f21de442287d7c8958f8062c72e210
Phanukorn <60050230@kmitl.ac.th>
2019-11-06 11:01:06 +0700

    Profile v.1.1

--- app/src/main/java/com/example/seapp/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/seapp/ui/profile/ProfileFragment.java
@@ -5,12 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.seapp.MainActivity;
@@ -19,6 +19,14 @@ import com.example.seapp.R;
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
+    private Button commit;
+    private TextView nameTxt;
+    private TextView nameLength;
+    private TextView statusLength;
+    private EditText name;
+    private EditText status;
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -29,23 +37,43 @@ public class ProfileFragment extends Fragment {
         profileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        //final TextView textView = root.findViewById(R.id.text_profile);
+
         final Button edit = root.findViewById(R.id.edit_btn);
+        nameTxt =(TextView)root.findViewById(R.id.nameText);
+        name =(EditText)root.findViewById(R.id.nameEdit);
+        status =(EditText)root.findViewById(R.id.statusEdit);
+        nameLength=(TextView)root.findViewById(R.id.nameLength);
+        statusLength=(TextView)root.findViewById(R.id.statusLength);
+        commit = (Button)root.findViewById(R.id.commit_btn);
+
 
 
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+            name.setBackgroundResource(R.drawable.border);
+            status.setBackgroundResource(R.drawable.border);
+            nameTxt.setText("ชื่อผู้ใช้ (ห้ามตั้งชื่อที่เข้าข่ายลามกอนาจาร)");
+            name.setFocusableInTouchMode(true);
+            status.setFocusableInTouchMode(true);
 
             }
         });
-        /*profileViewModel.getText().observe(this, new Observer<String>() {
+
+        commit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                name.setBackgroundResource(R.drawable.edittext_grey);
+                status.setBackgroundResource(R.drawable.edittext_grey);
+                nameTxt.setText("ชื่อผู้ใช้");
+                name.setFocusableInTouchMode(false);
+                status.setFocusableInTouchMode(false);
+                //Toast.makeText(getContext(),"บันทึกสำเร็จ",1000).show();
             }
-        });*/
+        });
+
+
+
         return root;
     }
 }
