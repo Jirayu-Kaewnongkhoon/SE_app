6f92a5bba092563ab5123130eb27fb0f6217b81f
aod <korarit1@gmail.com>
2019-11-08 18:21:25 +0700

    yolo

---
+++ app/src/main/java/com/example/seapp/ui/intro/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.seapp.ui.intro;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.seapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/seapp/ui/intro/lastIntroActivity.java
@@ -0,0 +1,18 @@
+package com.example.seapp.ui.intro;
+
+import android.os.Bundle;
+import android.view.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.seapp.R;
+
+public class lastIntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_last_intro);
+    }
+}
