532e3f6679d62f9798759fdb50d1afdd87706e24
Jirayu-Kaewnongkhoon <samurai_ice007@hotmail.com>
2019-11-15 14:31:42 +0700

    add FireBaseAuthentication ForgotPassword

--- app/src/main/java/com/example/seapp/ForgotPassword.java
+++ app/src/main/java/com/example/seapp/ForgotPassword.java
@@ -6,14 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPassword extends AppCompatActivity {
 
     private Dialog dlReset;
     private Button confirm;
     private EditText email;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +30,38 @@ public class ForgotPassword extends AppCompatActivity {
         email = findViewById(R.id.etEmailForgotPass);
 
         dlReset = new Dialog(this);
+
+        confirm = findViewById(R.id.btnSendEmailForgotPass);
+        mAuth = FirebaseAuth.getInstance();
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                if(email.getText().toString().trim().isEmpty()){
+                    Toast.makeText(getApplication(), "กรุณากรอกข้อมูลให้ครบ", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    mAuth.sendPasswordResetEmail(email.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                confirm = dlReset.findViewById(R.id.btnSendEmailForgotPass);
+                                ShowPopup(view);
+                                //Toast.makeText(getApplication(), "ตรวจสอบรายละเอียดในอีเมลของคุณ", Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                Toast.makeText(getApplication(), "เกิดข้อผิดพลาด", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
     }
     public void ShowPopup(View v){
 
         dlReset.setContentView(R.layout.activity_password_sent);
-        confirm = dlReset.findViewById(R.id.btnSendEmailForgotPass);
+        //confirm = dlReset.findViewById(R.id.btnSendEmailForgotPass);
 
         dlReset.show();
 
